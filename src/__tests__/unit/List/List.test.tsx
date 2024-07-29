71e27b37c49a2ef13045450aa6bfaf8044e6a94e
nidhinraj <phoenix.nidhin@gmail.com>
2024-07-29 09:08:20 +0530

    added unit test for list item

--- src/__tests__/unit/List/List.test.tsx
+++ src/__tests__/unit/List/List.test.tsx
@@ -15,16 +15,20 @@
 
 import React from 'react';
 import {
+  act, screen,
   cleanup, render,
   waitFor,
 } from '@testing-library/react';
 import { ThemeProvider } from '@emotion/react';
-
-import { ThemeDirectionType, ThemeModeType, createEnchantedTheme } from '../../../theme';
+import userEvent from '@testing-library/user-event';
+import {
+  ThemeDirectionType, ThemeModeType, createEnchantedTheme, ensureToGetColor,
+} from '../../../theme';
 import ListItem from '../../../List/ListItem';
 import List from '../../../List/List';
 import ListItemText from '../../../List/ListItemText';
-import ListItemButton from '../../../List/ListItemButton';
+import ListItemButton, { ListSizes } from '../../../List/ListItemButton';
+import { ColorNames, Colors } from '../../../colors';
 
 afterEach(cleanup);
 
@@ -67,6 +71,50 @@ describe('ListItem Component', () => {
       const style = window.getComputedStyle(listItemButton);
       expect(style.border).toBe('0px');
       expect(style.padding).toBe('6px 8px');
+    });
+  });
+
+  it('should check focus style', async () => {
+    const user = userEvent.setup();
+    render(
+      <ThemeProvider theme={createEnchantedTheme(ThemeDirectionType.LTR, ThemeModeType.LIGHT_NEUTRAL_GREY)}>
+        <ListItemButton data-testid="list-item-button" tabIndex={0}>
+          <ListItemText
+            primary="primary text"
+            secondary="secondary text"
+            data-testid="list-item-text"
+          />
+        </ListItemButton>
+      </ThemeProvider>,
+    );
+    const listItemButton = screen.getByTestId('list-item-button');
+    await act(async () => {
+      await user.tab();
+    });
+
+    await waitFor(() => {
+      const style = window.getComputedStyle(listItemButton);
+      expect(style.boxShadow).toBe(`0 0 0 1px ${(ensureToGetColor(Colors.get(ColorNames.HCLSOFTWAREBLUE07)))} inset`);
+    });
+  });
+
+  it('should check size variant', async () => {
+    render(
+      <ThemeProvider theme={createEnchantedTheme(ThemeDirectionType.LTR, ThemeModeType.LIGHT_NEUTRAL_GREY)}>
+        <ListItemButton data-testid="list-item-button" size={ListSizes.MEDIUM}>
+          <ListItemText
+            primary="primary text"
+            secondary="secondary text"
+            data-testid="list-item-text"
+          />
+        </ListItemButton>
+      </ThemeProvider>,
+    );
+    const listItemButton = screen.getByTestId('list-item-button');
+
+    await waitFor(() => {
+      const style = window.getComputedStyle(listItemButton);
+      expect(style.padding).toBe('6px 12px');
     });
   });
 });
