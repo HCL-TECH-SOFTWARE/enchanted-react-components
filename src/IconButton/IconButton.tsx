d8066ec877f7354aa650eff9a3db2d8203ef4ccb
Kristine Apalla <kristinegazel.apall@hcl.com>
2024-08-29 18:35:26 +0800

    update comment

--- src/IconButton/IconButton.tsx
+++ src/IconButton/IconButton.tsx
@@ -161,6 +161,9 @@ export const getMuiIconButtonThemeOverrides = (): Components<Omit<Theme, 'compon
  * @property {IconButtonSizes} size - The size of the component
  * @property {IconButtonVariants} variant - Adds padding around icon svg
  * @property {string} color - The color of the component.
+ * @property {boolean} selected - Set the component as selected.
+ * @property {string} label - Label of the component.
+ * @property {boolean} showendicon - Show/hide the end icon.
  */
 export type IconButtonProps = MuiIconButtonProps & {
   size?: string,
