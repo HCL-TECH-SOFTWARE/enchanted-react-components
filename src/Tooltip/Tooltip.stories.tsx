d114944c9f2134f8b0c1341f742980b02f7a350d
Omkumar Bhagat <omkumarhiralal.bhaga@hcl.com>
2024-10-07 11:30:38 +0530

    Removed unwanted imports

--- src/Tooltip/Tooltip.stories.tsx
+++ src/Tooltip/Tooltip.stories.tsx
@@ -14,7 +14,7 @@
  * ======================================================================== */
 import React from 'react';
 import { StoryFn, Meta } from '@storybook/react';
-import { Grid, Box, Typography } from '@mui/material';
+import { Grid } from '@mui/material';
 import Tooltip, {
   TooltipPlacement, TooltipSizes, TooltipTypes,
 } from './Tooltip';
