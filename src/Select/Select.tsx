acec748ee4b49237a58e002840a401f519c36ea6
MuhammedShabeelk <166281057+MuhammedShabeelk@users.noreply.github.com>
2025-01-22 15:04:22 +0530

    Screen reader accessibility is fixed on select component (#95)
    * accessibility is fixed on select
    
    * menuProps is added on select

--- src/Select/Select.tsx
+++ src/Select/Select.tsx
@@ -203,6 +203,7 @@ const renderInput = (props: SelectProps, id?: string) => {
     <MuiSelect
       {...selectProps}
       MenuProps={{
+        ...selectProps.MenuProps,
         transformOrigin: { vertical: 'top', horizontal: theme.direction === ThemeDirectionType.RTL ? 'right' : 'left' },
         anchorOrigin: { vertical: 'top', horizontal: theme.direction === ThemeDirectionType.RTL ? 'right' : 'left' },
         PaperProps: {
